ab84e97a16ec327c8c2d95ca5d982b80a89b170f
Tomsho7 <66315920+Tomsho7@users.noreply.github.com>
2020-06-07 05:45:00 +1300

    Add files via upload

---
+++ tomsho7.js
@@ -0,0 +1,7 @@
+let fullName = 'Oluwatomisin Shonibare';
+let ID = 'HNG-04551';
+let language = 'Javascript';
+let email = "tomisinshonibare@gmail.com";
+
+console.log('Hello World, this is ' + fullName + ' with HNGi7 ID ' + ID + ' using ' +
+language + ' for stage 2 task and ' + email);
